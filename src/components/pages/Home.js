c663c84240d4b0efbc423927c17280319916cb0d
Jay Patel <jay.patel@fidacity.com>
2023-01-22 00:46:45 -0500

    adding the temporary code

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Home.css';
 import farmsaLogo from './images/farmsaLogo.png';
+import ReactDOM from 'react-dom';
+import { SocialIcon } from 'react-social-icons';
 import background from './images/background.jpg';
 
 
@@ -12,6 +14,13 @@ export default function Home() {
         <div className='content'>
           <img src={farmsaLogo} alt="BigCo Inc. logo"/>
           <h1 className='title'>Financial Analysis and Risk Management Student Association</h1>
+          <div className='socials'>
+            <SocialIcon classname='icons' url="www.google.com" network="facebook" style={{ margin: 5}} />
+            <SocialIcon classname='icons' url="www.google.com" network="instagram" style={{ margin: 5}}/>
+            <SocialIcon classname='icons' url="www.google.com" network="discord" style={{ margin: 5}}/>
+            <SocialIcon classname='icons' url="www.google.com" network="twitter" style={{ margin: 5}}/>
+            <SocialIcon classname='icons' url="www.google.com" network="linkedin" style={{ margin: 5}}/>
+          </div>
         </div>
       </div>
     </>
--- src/components/pages/Team.js
+++ src/components/pages/Team.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import './Team.css';
+import background from './images/background.jpg';
 
 export default function Team() {
   return (
     <>
       <div className='team'>
-        <h1>MEET THE TEAM</h1>
-        <div className='presidents'>
-          <h2>CO-PRESIDENTS</h2>
+        <div className='content'>
+          <h1>MEET THE TEAM</h1>
+          <hr />
+          <div className='presidents'>
+            <h2>CO-PRESIDENTS</h2>
+            <div className='members'>
+              <div class="card">
+                <div class="box">
+                  <img src={background} class= "image" alt="" />
+                  <div class="name">First Name</div>
+                  <div class="position">Presidents</div>
+                  <a href="www.linkedin.com">LinkedIn</a>
+                </div>
+              </div>
+              <div class="card">
+                <div class="box">
+                  <img src={background} class= "image" alt="" />
+                  <div class="name">First Name</div>
+                  <div class="position">Presidents</div>
+                  <a href="www.linkedin.com">LinkedIn</a>
+                </div>
+              </div>
+              <div class="card">
+                <div class="box">
+                  <img src={background} class= "image" alt="" />
+                  <div class="name">First Name</div>
+                  <div class="position">Presidents</div>
+                  <a href="www.linkedin.com">LinkedIn</a> 
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        
-        
+  
       </div>
     </>
   );
