5005a52a718225f0473249a6ed1e3983dadc5541
Rodeoleo <64102303+Rodeoleo@users.noreply.github.com>
2024-03-17 17:42:16 -0400

    Update teams page (#11)
    * Create CNAME
    
    * Update teams page
    
    ---------
    
    Co-authored-by: UWFarmsa <123018678+UWFarmsa@users.noreply.github.com>

--- src/components/pages/Team.js
+++ src/components/pages/Team.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Team.css';
 import profile from './images/akshat.jpg';
+import pfp from './images/profile-placeholder.jpg';
 
 export default function Team() {
   return (
@@ -9,18 +10,48 @@ export default function Team() {
         <div class="team-row-title">
           <h1>OUR TEAM</h1>
         </div>
-        <div class="row">
+        <div className="row">
 
-          <div class="column">
-            <div class="card">
-              <div class="img-container">
-                <img src={profile} />
+          <div className="column">
+            <div className="card">
+              <div className="img-container">
+                <img src={pfp} />
               </div>
-              <h3>Akshat Suri</h3>
+              <h3>Akam Pabla</h3>
               <p>Co-President</p>
-              <div class="icons">
+              <div className="icons">
                 <a href="#">
-                  <i class="fab fa-linkedin"></i>
+                  <i className="fab fa-linkedin"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="card">
+              <div className="img-container">
+                <img src={pfp} />
+              </div>
+              <h3>Jacob Gomes</h3>
+              <p>Co-President</p>
+              <div className="icons">
+                <a href="#">
+                  <i className="fab fa-linkedin"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="card">
+              <div className="img-container">
+                <img src={pfp} />
+              </div>
+              <h3>Nelson Li</h3>
+              <p>Co-President</p>
+              <div className="icons">
+                <a href="#">
+                  <i className="fab fa-linkedin"></i>
                 </a>
               </div>
             </div>
